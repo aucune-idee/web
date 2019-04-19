c06e035274c5d5a2a8d1108b1fbc78756a5f65ea
Kamule42 <benjamin.herbomez@gmail.com>
2019-04-19 17:10:37 +0200

    Make navbar responsive

--- src/app/main/nav-bar/nav-bar.component.ts
+++ src/app/main/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
+  @Input()
+  public isTransparent:boolean=false;
+
+  public isBurgerActive:boolean=false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  public toogleBurger():void{
+    this.isBurgerActive = !this.isBurgerActive;
+  }
 }
