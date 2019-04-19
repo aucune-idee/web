f8673f0f5326da485ce0203b4b2f0244cacae9b2
Kamule42 <benjamin.herbomez@gmail.com>
2019-04-19 15:35:48 +0200

    Add site master template and about page

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -4,8 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent}
-  //{ path: '**', loadChildren: './home/home.module#HomeModule'}
+  { path: '', component: HomeComponent},
+  { path: '**', loadChildren: './site/site.module#SiteModule'}
 ];
 
 @NgModule({
---
+++ src/app/main/site/site-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  { path: '', component: MainComponent, children:[
+    { path: 'about', component: AboutComponent}
+  ]}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class SiteRoutingModule { }
