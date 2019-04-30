c500175fa1a387b2efcfa6b3fb306eb6f0a3d252
Kamule <benjamin.herbomez@gmail.com>
2019-04-30 16:20:48 +0200

    Add log info

--- dist/serve.js
+++ dist/serve.js
@@ -5,5 +5,8 @@ app.use(express.static('app'));
 
 let port = process.env.PORT ? process.env.port : 8080;
 app.listen(port, ()=>{
-    console.log("listening to "+port);
+    const host = server.address().address;
+    const port = server.address().port;
+  
+    console.log(`Example app listening at http://${host}:${port}`);
 });
