1556731378b3938d5aa4c3c48d28302024f995bf
Kamule <benjamin.herbomez@gmail.com>
2019-05-14 21:01:52 +0200

    Add redirect on signin

--- src/app/main/signin/signin.component.ts
+++ src/app/main/signin/signin.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router'
+import { Router, ActivatedRoute } from '@angular/router'
+
+import { filter } from 'rxjs/operators';
 
 import { SessionService, SigninInput, SigninOutput } from '@services/session';
+
 
 @Component({
   selector: 'app-signin',
@@ -12,18 +15,29 @@ export class SigninComponent implements OnInit {
 
   public data:SigninInput = {id:"",password: ""}
 
+  private redirect:String;
+
   constructor(
     private service:SessionService,
-    private router:Router) {}
+    private router:Router,
+    private route:ActivatedRoute) {
+    }
 
   ngOnInit() {
+    this.redirect = null;
+    this.route.queryParams.pipe(
+      filter(params => params.redirect)
+    )
+    .subscribe(param => {
+      this.redirect = param.redirect
+    })
   }
 
 
   onSubmit():void{
     this.service.auth(this.data).subscribe(
       (output:SigninOutput)=> {
-        this.router.navigate(['/']);
+        this.router.navigate(this.redirect ? [this.redirect] : ['/']);
       },
       (error:any)=>console.error(error)
     );
